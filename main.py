1ac101b7e851681e362b88eb651a87a2de61aba4
Jcoumarb <jitonri@gmail.com>
2024-04-26 11:03:01 -0400

    Add setup for a Better Developer Environment

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ app = FastAPI()
 # Defines API route
 @app.get("/api/v1/hello")
 async def api():
-    return {"message": "Hello World"}
+    return {"message": "Hello Coders. I'm literally Mark Zuckerberg"}
 
 
 # Defines root route to UI
-#@app.get("/")
-#async def root():
+# @app.get("/")
+# async def root():
 #    return {"message": "Welcome to the UI (more functionality to come"}
 
 app.mount("/", StaticFiles(directory="ui/dist", html=True), name="ui")
